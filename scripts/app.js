07157d359eeb2490b836e6e48c912afabeab8d0a
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-08 22:17:58 -0500

    retrieves data from form

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,49 @@
+// formDataObject
+var formDataObject = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  company: '',
+  title: '',
+  country: '',
+  state: '',
+  comments: ''
+};
+
+// document ready
 $(document).ready(function(){
   console.log("I'm Here For You");
+  eventListener();
 });
+
+// eventListener
+function eventListener(){
+  $( '#infoForm' ).on( 'submit', function(event){
+    event.preventDefault();
+    console.log("you've clicked submit");
+    gatherData();
+  });
+}
+
+// gatherData
+function gatherData(){
+  console.log("you are inside gatherData");
+  console.log("here is your object", formDataObject);
+  formDataObject.firstName = $('#inputFirstName').val();
+  formDataObject.lastName = $('#inputLastName').val();
+  formDataObject.email = $('#inputEmail').val();
+  formDataObject.company = $('#inputCompany').val();
+  formDataObject.title = $('#inputTitle').val();
+  formDataObject.country = $('#inputCountry').val();
+  formDataObject.state = $('#inputState').val();
+  formDataObject.comments = $('#inputComments').val();
+  console.log("here is your object again", formDataObject);
+  displayFormData();
+}
+
+// display object
+function displayFormData(){
+  console.log("you are inside displayFormData");
+}
+
+// clear form
