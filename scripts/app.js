96df080305e404e0d11184cf3625e2a6e0391c25
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-09 10:57:13 -0500

    sets up jQuery logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-// formDataObject
+// global var should be an instance of a class
 var formDataObject = {
   firstName: '',
   lastName: '',
@@ -22,25 +22,25 @@ function eventListener(){
     event.preventDefault();
     console.log("you've clicked submit");
     gatherData();
+    displayTable();
+  });
+
+  $( '#saveAndClear').on( 'click', function(){
+    console.log("your clicked saveAndClear");
+    saveData();
+    clearForm();
+    hideTable();
+  });
+
+  $( '#deleteAndClear').on( 'click', function(){
+    console.log("your clicked deleteAndClear");
+    clearForm();
+    hideTable();
   });
 }
 
 // gatherData
 function gatherData(){
-
-  var formDataObject = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    company: '',
-    title: '',
-    country: '',
-    state: '',
-    comments: ''
-  };
-
-  console.log("you are inside gatherData");
-  console.log("here is your object", formDataObject);
   formDataObject.firstName = $('#inputFirstName').val();
   formDataObject.lastName = $('#inputLastName').val();
   formDataObject.email = $('#inputEmail').val();
@@ -49,13 +49,57 @@ function gatherData(){
   formDataObject.country = $('#inputCountry').val();
   formDataObject.state = $('#inputState').val();
   formDataObject.comments = $('#inputComments').val();
-  console.log("here is your object again", formDataObject);
-  displayFormData(formDataObject);
 }
 
 // display object
-function displayFormData(formDataObject){
-  console.log("you are inside displayFormData", formDataObject);
+function displayTable(){
+  console.log("you are inside displayTable");
+  $.each(formDataObject, function(key, value){
+    console.log("key then value", key, value);
+    if (value === ''){
+      //hide the row
+    }
+    else{
+      //append value to the table
+    }
+  });//ends each loop
+
+}
+
+// save data
+function saveData(){
+  // if we were to be sending this data to a server, we would do it here
+  console.log("inside saveData");
+  emptyFormDataObject();
+}
+
+// empty data object
+function emptyFormDataObject(){
+  console.log("inside emptyFormDataObject");
+  formDataObject.firstName = '';
+  formDataObject.lastName = '';
+  formDataObject.email = '';
+  formDataObject.company = '';
+  formDataObject.title = '';
+  formDataObject.country = '';
+  formDataObject.state = '';
+  formDataObject.comments = '';
 }
 
 // clear form
+function clearForm(){
+  console.log("inside clearForm");
+  $('#inputFirstName').val('');
+  $('#inputLastName').val('');
+  $('#inputEmail').val('');
+  $('#inputCompany').val('');
+  $('#inputTitle').val('');
+  $('#inputCountry').val('');
+  $('#inputState').val('');
+  $('#inputComments').val('');
+}
+
+// hide table
+function hideTable(){
+  console.log("inside hideTable");
+}
